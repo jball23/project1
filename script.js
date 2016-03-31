52c5c7c0a0679d84a5065a39f42abe0a06969afd
Jordan Ballard <ballard.jordan@gmail.com>
2016-03-31 08:19:29 -0400

    overhauled the code. Colors in array, shuffle colors, reset button, less global variables, etc.

--- script.js
+++ script.js
@@ -1,216 +1,135 @@
-var deck = document.querySelectorAll(".square");
-var color = '';
-var colorEven;
-var colorOdd;
-var defaultColor = 'lightblue';
-var card;
-var clicks = 0;
-var firstCardOfPair;
+var bgColor = ['#ce3c3c', '#ce3c3c', '#53BBF4', '#53BBF4',
+               '#e793f9', '#e793f9', '#77242c', '#77242c',
+               '#223a93', '#223a93', '#5d51fc', '#5d51fc',
+               '#0aef0a', '#0aef0a', '#1CDDBA', '#1CDDBA',
+               '#a3049b', '#a3049b', '#eaea28', '#eaea28',
+               '#eaaa28', '#eaaa28', '#0a7a0a', '#0a7a0a'];
 
-for(var i = 0; i < deck.length; i++) {
-  card = document.querySelectorAll(".square");
-  card[i].addEventListener("click", showColor);
+var clicks = 0;
+var matched = 0;
+var cards = document.querySelectorAll(".square");
+var resetButton = document.getElementsByTagName("button");
+
+
+
+// Shuffles the colors in my bgColor array
+// ***************************************
+function shuffle(array) {
+  var i;
+  var j;
+  var temp;
+  for(i = array.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * i);
+    temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
+shuffle(bgColor);
+
+
+// Adds a data-content attribute to index.html and gives each attribute
+// a random color from the bgColor array
+// ********************************************************************
+function setColors() {
+  for(var i = 0; i < cards.length; i++) {
+    cards[i].setAttribute("data-content", bgColor[i]);
+  }
+}
+setColors();
+
+
+// Adds a click event listener to each card and references the flipCard
+// function in order to show the bgColor assigned to it
+// ********************************************************************
+for(var i = 0; i < cards.length; i++) {
+  cards[i].addEventListener("click", flipCard);
 }
 
 
-function showColor() {
+// Flips the card over to reveal it's color, increases the click
+// count, and evaluates matches
+// ****************************************************************
+function flipCard() {
   var currentCard = this;
-
-  color = currentCard.getAttribute("data-content");
-  currentCard.style.background = color;
+  var background = this.getAttribute("data-content");
+  currentCard.style.background = background;
   clicks += 1;
 
   if(clicks % 2 === 0) {
-    colorEven = color;
+    colorEven = background;
   } else {
-    colorOdd = color;
+    colorOdd = background;
     firstCardOfPair = currentCard;
   }
 
   if(clicks % 2 === 0 && colorEven !== colorOdd) {
-    alert("no match");
-    firstCardOfPair.style.background = defaultColor;
-    currentCard.style.background = defaultColor;
+    var delay = 500; // mis-matched cards flip back over after delay
+    setTimeout(function() {
+      firstCardOfPair.style.background = 'white';
+      currentCard.style.background = 'white';
+    }, delay);
+  } else if(clicks % 2 === 0 && colorEven === colorOdd) {
+      matched++;
+      console.log(matched);
   }
 
+  if(matched === 12) {
+    alert("YOU'RE A WINNER, JUST LIKE YOUR MOM SAID!!");
+      }
 }
 
 
+// Reset the colors on the board to white, reset the click
+// count to zero, and shuffle the cards
+// *******************************************************
+function resetBoard() {
+  for(var i = 0; i < cards.length; i++) {
+    cards[i].style.background = 'white';
+  }
+  clicks = 0;
+  shuffle(bgColor);
+  setColors();
+}
+
+resetButton[0].addEventListener("click", resetBoard);
 
 
+//**************  OLD CODE!  ****************
+//**************  OLD CODE!  ****************
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var squares = document.querySelectorAll(".square");
-// var clicks = 1;
-// var lastPicked;
+// var deck = document.querySelectorAll(".square");
+// var color = '';
+// var colorEven;
+// var colorOdd;
+// var defaultColor = 'white';
+// var card;
+// var clicks = 0;
+// var firstCardOfPair;
 //
-//
-//
-// for(var i = 0; i < squares.length; i++) {
-//   squares[i].addEventListener("click", function() {
-//     this.style.background = this.getAttribute("data-content");
-// //compare the two cards
-//
-//
-//
-//     lastPicked = this;
-//     // if(lastPicked.style.background = this.getAttribute("data-content")) {
-//     //   console.log("matched");
-//     // }
-//
-//     clicks++;
-//   });
+// for(var i = 0; i < deck.length; i++) {
+//   card = document.querySelectorAll(".square");
+//   card[i].addEventListener("click", showColor);
 // }
 //
-
-
-
 //
-// var square1 = document.getElementById("square-1");
-// var square2 = document.getElementById("square-2");
-// var square3 = document.getElementById("square-3");
-// var square4 = document.getElementById("square-4");
-// var square5 = document.getElementById("square-5");
-// var square6 = document.getElementById("square-6");
-// var square7 = document.getElementById("square-7");
-// var square8 = document.getElementById("square-8");
-// var square9 = document.getElementById("square-9");
-// var square10 = document.getElementById("square-10");
-// var square11 = document.getElementById("square-11");
-// var square12 = document.getElementById("square-12");
-// var square13 = document.getElementById("square-13");
-// var square14 = document.getElementById("square-14");
-// var square15 = document.getElementById("square-15");
-// var square16 = document.getElementById("square-16");
-// var square17 = document.getElementById("square-17");
-// var square18 = document.getElementById("square-18");
-// var square19 = document.getElementById("square-19");
-// var square20 = document.getElementById("square-20");
-// var square21 = document.getElementById("square-21");
-// var square22 = document.getElementById("square-22");
-// var square23 = document.getElementById("square-23");
-// var square24 = document.getElementById("square-24");
+// function showColor() {
+//   var currentCard = this;
 //
-// var color1 = "blue";
-// var color2 = "red";
-// var color3 = "green";
-// var color4 = "gold";
-// var color5 = "purple";
-// var color6 = "orange";
-// var color7 = "magenta";
-// var color8 = "cyan";
-// var color9 = "lightblue";
-// var color10 = "lightgreen";
-// var color11 = "lightyellow";
-// var color12 = "lime";
+//   color = currentCard.getAttribute("data-content");
+//   currentCard.style.background = color;
+//   clicks += 1;
 //
+//   if(clicks % 2 === 0) {
+//     colorEven = color;
+//   } else {
+//     colorOdd = color;
+//     firstCardOfPair = currentCard;
+//   }
 //
-// square1.addEventListener("click", squareColor1);
-// square2.addEventListener("click", squareColor2);
-// square3.addEventListener("click", squareColor3);
-// square4.addEventListener("click", squareColor4);
-// square5.addEventListener("click", squareColor5);
-// square6.addEventListener("click", squareColor6);
-// square7.addEventListener("click", squareColor7);
-// square8.addEventListener("click", squareColor8);
-// square9.addEventListener("click", squareColor9);
-// square10.addEventListener("click", squareColor10);
-// square11.addEventListener("click", squareColor11);
-// square12.addEventListener("click", squareColor12);
-// square13.addEventListener("click", squareColor8);
-// square14.addEventListener("click", squareColor3);
-// square15.addEventListener("click", squareColor9);
-// square16.addEventListener("click", squareColor2);
-// square17.addEventListener("click", squareColor7);
-// square18.addEventListener("click", squareColor10);
-// square19.addEventListener("click", squareColor1);
-// square20.addEventListener("click", squareColor6);
-// square21.addEventListener("click", squareColor12);
-// square22.addEventListener("click", squareColor11);
-// square23.addEventListener("click", squareColor4);
-// square24.addEventListener("click", squareColor5);
-//
-//
-// function squareColor1() {
-//   this.style.backgroundColor = color1;
-// }
-//
-// function squareColor2() {
-//   this.style.backgroundColor = color2;
-// }
-//
-// function squareColor3() {
-//   this.style.backgroundColor = color3;
-// }
-//
-// function squareColor4() {
-//   this.style.backgroundColor = color4;
-// }
-//
-// function squareColor5() {
-//   this.style.backgroundColor = color5;
-// }
-//
-// function squareColor6() {
-//   this.style.backgroundColor = color6;
-// }
-//
-// function squareColor7() {
-//   this.style.backgroundColor = color7;
-// }
-//
-// function squareColor8() {
-//   this.style.backgroundColor = color8;
-// }
-//
-// function squareColor9() {
-//   this.style.backgroundColor = color9;
-// }
-//
-// function squareColor10() {
-//   this.style.backgroundColor = color10;
-// }
-//
-// function squareColor11() {
-//   this.style.backgroundColor = color11;
-// }
-//
-// function squareColor12() {
-//   this.style.backgroundColor = color12;
+//   if(clicks % 2 === 0 && colorEven !== colorOdd) {
+//     alert("no match");
+//     firstCardOfPair.style.background = defaultColor;
+//     currentCard.style.background = defaultColor;
+//   }
 // }
